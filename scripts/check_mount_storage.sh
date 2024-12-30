b5f4a7274cbc68d3ba9e1a0695166bbdcab6230b
johann8 <jo.hahn@gmx.de>
2024-12-30 22:11:09 +0100

    added option -w to grep

--- scripts/check_mount_storage.sh
+++ scripts/check_mount_storage.sh
@@ -5,7 +5,7 @@
 #
 
 # Set script version
-SCRIPT_VERSION="0.1.0"
+SCRIPT_VERSION="0.2"
 
 # CUSTOM Vars
 MOUNT_NAME=BareOS                          # The name of mount point - case sensitive | df -hT
@@ -13,7 +13,7 @@ MOUNT_PATH=/mnt/NAS_BareOS                 # Mount point PATH | df -hT
 STORAGE_NAME=bacula                        # Name of storage folder | ls -la ${MOUNT_PATH}
 
 # check mount path
-MOUNT_PATH_IDENTIFIED=$(cat /proc/mounts |grep ${MOUNT_NAME} | awk '{print $2}')
+MOUNT_PATH_IDENTIFIED=$(cat /proc/mounts |grep -w ${MOUNT_NAME} | awk '{print $2}')
 
 if [[ $(df -h |grep ${MOUNT_NAME}) ]] && [[ ${MOUNT_PATH} = ${MOUNT_PATH_IDENTIFIED} ]] && [[ -d ${MOUNT_PATH}/${STORAGE_NAME}/archive ]]; then
    # Bacula can be started
