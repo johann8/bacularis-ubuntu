7ad70d4c11f9f5df25d4966dcf5999055e1f4dbe
johann8 <jo.hahn@gmx.de>
2026-06-19 12:21:51 +0200

    changed docker image version to 6.3.0; bacularis version to 6.3.0

--- build_image_24.04.sh
+++ build_image_24.04.sh
@@ -1,22 +1,24 @@
 #!/bin/bash
 
 # set variables
-# set variables
-D_IMAGE_VERSION=6.2.1
-PHP_VERSION=8.3
-BACULA_VERSION=15.0.3
-BACULARIS_VERSION=6.2.1
+D_IMAGE_VERSION=6.3.0
+D_IMAGE_TAG=ubuntu
+BASE_IMAGE="ubuntu:noble"
 UBUNTU_VERSION=24.04
 
+PHP_VERSION=8.3
+BACULARIS_VERSION=6.3.0
+BACULA_VERSION=15.0.3
 
-# build docker image
+# create build docker image
 # docker build -t johann8/bacularis:${_VERSION}-ubuntu . 2>&1 | tee ./build.log
 docker build \
+  --build-arg=BASE_IMAGE=${BASE_IMAGE} \
   --build-arg=BACULARIS_VERSION=${BACULARIS_VERSION} \
   --build-arg=BACULA_VERSION=${BACULA_VERSION} \
   --build-arg=PHP_VERSION=${PHP_VERSION} \
   --platform=linux/amd64 \
-  --tag=johann8/bacularis:${D_IMAGE_VERSION}-ubuntu-${UBUNTU_VERSION} \
+  --tag=johann8/bacularis:${D_IMAGE_VERSION}-${D_IMAGE_TAG}-${UBUNTU_VERSION} \
   --file=./Dockerfile_U24.04 . 2>&1 | tee ./build.log
 
 _BUILD=$?
@@ -27,14 +29,14 @@ if ! [ ${_BUILD} = 0 ]; then
 else
    echo "Docker Image build successful"
    docker images -a 
-   docker tag johann8/bacularis:${D_IMAGE_VERSION}-ubuntu-${UBUNTU_VERSION} johann8/bacularis:latest-ubuntu-${UBUNTU_VERSION}
+   docker tag johann8/bacularis:${D_IMAGE_VERSION}-${D_IMAGE_TAG}-${UBUNTU_VERSION} johann8/bacularis:latest-${D_IMAGE_TAG}-${UBUNTU_VERSION}
 fi
 
 #push image to dockerhub
 if [ ${_BUILD} = 0 ]; then
    echo "Pushing docker images to dockerhub..."
-   docker push johann8/bacularis:latest-ubuntu-${UBUNTU_VERSION}
-   docker push johann8/bacularis:${D_IMAGE_VERSION}-ubuntu-${UBUNTU_VERSION}
+   docker push johann8/bacularis:latest-${D_IMAGE_TAG}-${UBUNTU_VERSION}
+   docker push johann8/bacularis:${D_IMAGE_VERSION}-${D_IMAGE_TAG}-${UBUNTU_VERSION}
    _PUSH=$?
    docker images -a |grep bacularis
 fi
@@ -42,8 +44,8 @@ fi
 #delete build
 if [ ${_PUSH=} = 0 ]; then
    echo "Deleting docker images..."
-   docker rmi johann8/bacularis:latest-ubuntu-${UBUNTU_VERSION}
-   docker rmi johann8/bacularis:${D_IMAGE_VERSION}-ubuntu-${UBUNTU_VERSION}
+   docker rmi johann8/bacularis:latest-${D_IMAGE_TAG}-${UBUNTU_VERSION}
+   docker rmi johann8/bacularis:${D_IMAGE_VERSION}-${D_IMAGE_TAG}-${UBUNTU_VERSION}
    docker images -a
    #docker rmi ubuntu
 fi
