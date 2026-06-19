6c222290085c8d1180a224ee0a99e6cd265c9804
johann8 <jo.hahn@gmx.de>
2026-06-19 12:21:31 +0200

    changed docker image version to 6.3.0; bacularis version to 6.3.0x!

--- build_image.sh
+++ build_image.sh
@@ -1,19 +1,25 @@
 #!/bin/bash
 
 # set variables
-D_IMAGE_VERSION=6.2.1
-PHP_VERSION=8.1
-BACULA_VERSION=15.0.3
-BACULARIS_VERSION=6.2.1
+D_IMAGE_VERSION=6.3.0
+D_IMAGE_TAG=ubuntu
+BASE_IMAGE="ubuntu:jammy"
 UBUNTU_VERSION=22.04
 
-# build docker image
+PHP_VERSION=8.1
+BACULARIS_VERSION=6.3.0
+BACULA_VERSION=15.0.3
+
+
+# create build docker image
+#docker build -f ./Dockerfile -t johann8/bacularis:${D_IMAGE_VERSION}-alpine . 2>&1 | tee ./build.log
 docker build \
+  --build-arg=BASE_IMAGE=${BASE_IMAGE} \
   --build-arg=BACULARIS_VERSION=${BACULARIS_VERSION} \
   --build-arg=BACULA_VERSION=${BACULA_VERSION} \
   --build-arg=PHP_VERSION=${PHP_VERSION} \
   --platform=linux/amd64 \
-  --tag=johann8/bacularis:${D_IMAGE_VERSION}-ubuntu \
+  --tag=johann8/bacularis:${D_IMAGE_VERSION}-${D_IMAGE_TAG} \
   --file=./Dockerfile . 2>&1 | tee ./build.log
 
 _BUILD=$?
@@ -25,14 +31,14 @@ if ! [ ${_BUILD} = 0 ]; then
 else
    echo "Docker Image build successful"
    docker images -a
-   docker tag johann8/bacularis:${D_IMAGE_VERSION}-ubuntu johann8/bacularis:latest-ubuntu
+   docker tag johann8/bacularis:${D_IMAGE_VERSION}-${D_IMAGE_TAG} johann8/bacularis:latest-${D_IMAGE_TAG}
 fi
 
 #push image to dockerhub
 if [ ${_BUILD} = 0 ]; then
    echo "Pushing docker images to dockerhub..."
-   docker push johann8/bacularis:latest-ubuntu
-   docker push johann8/bacularis:${D_IMAGE_VERSION}-ubuntu
+   docker push johann8/bacularis:latest-${D_IMAGE_TAG}
+   docker push johann8/bacularis:${D_IMAGE_VERSION}-${D_IMAGE_TAG}
    _PUSH=$?
    docker images -a |grep bacularis
 fi
@@ -40,8 +46,8 @@ fi
 #delete build
 if [ ${_PUSH=} = 0 ]; then
    echo "Deleting docker images..."
-   docker rmi johann8/bacularis:latest-ubuntu
-   docker rmi johann8/bacularis:${D_IMAGE_VERSION}-ubuntu
+   docker rmi johann8/bacularis:latest-${D_IMAGE_TAG}
+   docker rmi johann8/bacularis:${D_IMAGE_VERSION}-${D_IMAGE_TAG}
    docker images -a
    # docker rmi
 fi
